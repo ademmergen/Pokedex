e5de704e3899d8dc0c1715abaf5d88348764b8e9
amergen28 <adem.mergen@ogr.iuc.edu.tr>
2024-08-27 16:50:34 +0300

    inner tableView added

--- Pokemon/Modules/Detail/Cells/PokemonDetailTableViewCell.swift
+++ Pokemon/Modules/Detail/Cells/PokemonDetailTableViewCell.swift
@@ -5,6 +5,63 @@
 //  Created by Adem Mergen on 26.08.2024.
 //
 
+//import UIKit
+//
+//class PokemonDetailTableViewCell: UITableViewCell {
+//  
+//    @IBOutlet weak var propertyLabel: UILabel!
+//    @IBOutlet weak var valueLabel: UILabel!
+//    @IBOutlet weak var downButton: UIButton!
+//    @IBOutlet weak var innerTableView: UITableView!
+//  
+//    var isExpanded: Bool = false
+//    var expandedContent: [String] = []
+//    
+//    override func awakeFromNib() {
+//        super.awakeFromNib()
+//        downButton.addTarget(self, action: #selector(toggleExpand), for: .touchUpInside)
+//        innerTableView.delegate = self
+//        innerTableView.dataSource = self
+//        innerTableView.isHidden = true
+//    }
+//    
+//    @objc func toggleExpand() {
+//        isExpanded.toggle()
+//        innerTableView.isHidden = !isExpanded
+//        innerTableView.reloadData()
+//      print("basarili")
+//    }
+//    
+//    func configure(with title: String, value: String?) {
+//      propertyLabel.text = title
+//              valueLabel.text = value ?? ""
+//              valueLabel.isHidden = false // ValueLabel'ın görünür olmasını sağlıyoruz
+//              downButton.isHidden = true
+//              innerTableView.isHidden = true
+//    }
+//    
+//    func configure(with title: String, expandedContent: [String]) {
+//      propertyLabel.text = title
+//              valueLabel.isHidden = true // Expanded Content için ValueLabel'ı gizliyoruz
+//              downButton.isHidden = false
+//              self.expandedContent = expandedContent
+//              innerTableView.isHidden = !isExpanded
+//              innerTableView.reloadData()
+//    }
+//}
+//
+//extension PokemonDetailTableViewCell: UITableViewDelegate, UITableViewDataSource {
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return expandedContent.count
+//    }
+//    
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = UITableViewCell(style: .default, reuseIdentifier: "InnerCell")
+//        cell.textLabel?.text = expandedContent[indexPath.row]
+//        return cell
+//    }
+//}
+
 import UIKit
 
 class PokemonDetailTableViewCell: UITableViewCell {
@@ -14,39 +71,28 @@ class PokemonDetailTableViewCell: UITableViewCell {
     @IBOutlet weak var downButton: UIButton!
     @IBOutlet weak var innerTableView: UITableView!
   
-    var isExpanded: Bool = false
     var expandedContent: [String] = []
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        downButton.addTarget(self, action: #selector(toggleExpand), for: .touchUpInside)
         innerTableView.delegate = self
         innerTableView.dataSource = self
-        innerTableView.isHidden = true
-    }
-    
-    @objc func toggleExpand() {
-        isExpanded.toggle()
-        innerTableView.isHidden = !isExpanded
-        innerTableView.reloadData()
-      print("basarili")
     }
     
     func configure(with title: String, value: String?) {
-      propertyLabel.text = title
-              valueLabel.text = value ?? ""
-              valueLabel.isHidden = false // ValueLabel'ın görünür olmasını sağlıyoruz
-              downButton.isHidden = true
-              innerTableView.isHidden = true
+        propertyLabel.text = title
+        valueLabel.text = value ?? ""
+        valueLabel.isHidden = false
+        downButton.isHidden = true
+        innerTableView.isHidden = true
     }
     
     func configure(with title: String, expandedContent: [String]) {
-      propertyLabel.text = title
-              valueLabel.isHidden = true // Expanded Content için ValueLabel'ı gizliyoruz
-              downButton.isHidden = false
-              self.expandedContent = expandedContent
-              innerTableView.isHidden = !isExpanded
-              innerTableView.reloadData()
+        propertyLabel.text = title
+        valueLabel.isHidden = true
+        downButton.isHidden = false
+        self.expandedContent = expandedContent
+        innerTableView.reloadData()
     }
 }
 
@@ -60,8 +106,9 @@ extension PokemonDetailTableViewCell: UITableViewDelegate, UITableViewDataSource
         cell.textLabel?.text = expandedContent[indexPath.row]
         return cell
     }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 44
+    }
 }
-
-
-
 
